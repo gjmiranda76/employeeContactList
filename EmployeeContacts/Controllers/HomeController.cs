459a503bc40eab78cd5b48f8b07217471f6883fe
gjmiranda76 <guillermo.miranda@gmail.com>
2019-11-17 00:02:08 -0800

    WIP UI and controller stuff

--- EmployeeContacts/Controllers/HomeController.cs
+++ EmployeeContacts/Controllers/HomeController.cs
@@ -20,12 +20,7 @@ namespace EmployeeContacts.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return RedirectToAction("Index", "Employee");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
